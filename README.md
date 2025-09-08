14b4971be0a2fb9b0ea8b0409af1da8fda340292
Alexander <129068286+idutvuk@users.noreply.github.com>
2025-09-08 10:37:19 +0300

    replace argparse with typer

--- README.md
+++ README.md
@@ -1,13 +1,6 @@
 # RESHALLAh
-Утилита для решения примитивных вузовских задач по программированию при помощи LLM с поддержкой MCP и сборкой отчётов по ГОСТу через LaTeX.  Прозиносится как решалА. 
+Утилита для решения примитивных вузовских задач по программированию при помощи LLM с поддержкой MCP и сборкой отчётов по ГОСТу через Typst.  Прозиносится как решалА. 
 
 # Установка
-1. Клонируйте репозиторий
-2. Поставьте докер и запустите его
-3. Запустите через bash (git bash если вы на винде)
-```bash
-docker build -t tex-image . && \
-docker run --name tex-container tex-image && \
-docker cp tex-container:/template/main.pdf ./ && \
-docker rm tex-container
-```
+1. поставьте [uv](https://astral.sh/uv), если не стоит
+2. `uv run https://raw.githubusercontent.com/idutvuk/RESHALLAh/refs/heads/main/typst_compiler.py -d <путь к папке с проектом> -t <тип на выход> -o <имя файла без расширения>`
--- typst_compiler.py
+++ typst_compiler.py
@@ -2,46 +2,56 @@
 # requires-python = ">=3.13"
 # dependencies = [
 #     "typst",
+#     "typer",
 # ]
 # ///
+from enum import Enum
+from typing import List
+from typing_extensions import Annotated
 
-import argparse
-
+import typer
 import typst
-
 import tempfile
 import shutil
 import os
 
+class OutputFormat(str, Enum):
+    pdf = "pdf"
+    png = "png"
+    svg = "svg"
 
-def main():
-    parser = argparse.ArgumentParser(description="Compile typst files")
-    parser.add_argument("-f", "--files", nargs="*", help="Send files (.typ and all needed, space-separatd). Last .typ will be compiled")
-    parser.add_argument("-d", "--directory", nargs="?", help="Directory to process")
-    parser.add_argument("-o", "--output", help="Output filename (overrides!)", default="output")
-    parser.add_argument("-t", "--type-format", help="Export type", choices=["pdf", "png", "svg"], default="pdf")
-    args = parser.parse_args()
-    outfile_name=f"{args.output}.{args.type_format}"
+def main(
+        directory: Annotated[str, typer.Option("--dir", "-d")] = ".",
+        files: Annotated[List[str], typer.Option("--files", "-f")] = (),
+        output: Annotated[str, typer.Option("--output", "-o")] = "output",
+        type_format: Annotated[OutputFormat, typer.Option("--type_output", "-t")] = OutputFormat.pdf.value
+):
+    outfile_name = f"{output}.{type_format.value}"
 
     with tempfile.TemporaryDirectory() as temp_dir, open(outfile_name, "wb") as output_file:
-        if args.directory:
-            shutil.copytree(args.directory, temp_dir,  dirs_exist_ok=True)
+        if directory:
+            shutil.copytree(directory, temp_dir, dirs_exist_ok=True)
             typ_file = [f for f in os.listdir(temp_dir) if f.endswith(".typ")][0]
-        elif args.files:
-            for f in args.files:
+        elif len(files):
+            typer.echo(f"{files}, {len(files)}, {type(files)}")
+            for f in files:
                 shutil.copy(f, temp_dir)
                 if f.endswith(".typ"):
                     typ_file = f
         else:
-            raise ValueError("nothing provided")
+            typer.echo("no --directory or --files provided, lemme try to compile current directory")
+            shutil.copytree(os.getcwd(), temp_dir, dirs_exist_ok=True)
+            typ_file = [f for f in os.listdir(temp_dir) if f.endswith(".typ")][0]
+        
         output = typst.compile(
             os.path.join(temp_dir, typ_file),
-            format=args.type_format,
+            format=type_format.value,
             ppi=144.0
-            )
+        )
 
         output_file.write(output)
-        print(f"compiled, output at {outfile_name}")
+        typer.echo(f"compiled, output at {outfile_name}")
+
 
 if __name__ == "__main__":
-    main()
+    typer.run(main)
